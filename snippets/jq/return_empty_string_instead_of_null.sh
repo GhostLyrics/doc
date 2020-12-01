8d96b9d4383d3efbda211541775cc5f39336e3f1
Alexander Skiba <alexander.skiba@timetac.com>
2020-12-01 14:48:14 +0100

    add links to jq sources

--- snippets/jq/return_empty_string_instead_of_null.sh
+++ snippets/jq/return_empty_string_instead_of_null.sh
@@ -1,1 +1,3 @@
+# https://github.com/stedolan/jq/issues/354#issuecomment-46641827
+
 jq '.example | select (.!=null)' FILE
--- snippets/jq/return_result_without_double_quotes.sh
+++ snippets/jq/return_result_without_double_quotes.sh
@@ -1,1 +1,3 @@
+# source: https://github.com/stedolan/jq/issues/354#issuecomment-69663178
+
 jq --raw-output '.example' FILE
